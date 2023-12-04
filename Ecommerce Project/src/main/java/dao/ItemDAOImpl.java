eb5d6ce32afd48ea747eb01839dcd4648bd9048d
kloxer <102638907+kloxer@users.noreply.github.com>
2023-12-03 19:50:08 -0500

    Small edits

--- Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
+++ Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
@@ -1,84 +1,49 @@
 package dao;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletContext;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 import model.Item;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.ServletContext;
 
-public class ItemDAOImpl implements ItemDAO{
-	
-	private ServletContext context;
-	
+public class ItemDAOImpl implements ItemDAO {
+
+    private ServletContext context;
+
     public ItemDAOImpl(ServletContext context) {
         this.context = context;
     }
-    
-    private Connection getConnection() throws SQLException, ClassNotFoundException {
-    	Class.forName("org.sqlite.JDBC");
-    	String dbLocation = this.context.getRealPath("/dbFile/ecomDB.db");
-        return DriverManager.getConnection("jdbc:sqlite:" + dbLocation);
+
+    private Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new SQLException("MySQL Driver not found", e);
+        }
+        String url = "jdbc:mysql://ecomproj-ecomfin.a.aivencloud.com:18620/defaultdb";
+        String user = "avnadmin";
+        String password = "AVNS_4En2QrrZ4DU5tpub8TB";
+        return DriverManager.getConnection(url, user, password);
     }
 
     public List<Item> getAllItems() {
-	    List<Item> items = new ArrayList<>();
-	    try {
-	        // Load the JDBC driver class
-	        Class.forName("com.mysql.cj.jdbc.Driver"); // This is often optional in newer versions of JDBC
-
-	        // Set your database credentials and URL
-	        String url = "jdbc:mysql://ecomproj-ecomfin.a.aivencloud.com:18620/defaultdb";
-	        String user = "avnadmin";
-	        String password = "AVNS_4En2QrrZ4DU5tpub8TB";
-
-	        // Create a connection to the database
-	        Connection con = DriverManager.getConnection(url, user, password);
-	        
-	        // Create a statement object to execute queries
-	        Statement statement = con.createStatement();
-	        
-    	    ResultSet resultSet = statement.executeQuery("SELECT * FROM Item")) {
-                while (resultSet.next()) {
-                    Item item = new Item();
-                    item.setName(resultSet.getString("name"));
-                    item.setPrice(resultSet.getDouble("price"));
-                    items.add(item);
-                }
-            } 
-    	    catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
+        List<Item> items = new ArrayList<>();
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM Item")) {
+            while (resultSet.next()) {
+                Item item = new Item();
+                item.setName(resultSet.getString("name"));
+                item.setPrice(resultSet.getDouble("price"));
+                items.add(item);
             }
-            return items;
-    	    
-//        List<Item> items = new ArrayList<>();
-//        try (Connection connection = getConnection();
-//             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Item");
-//             ResultSet resultSet = statement.executeQuery()) {
-//            while (resultSet.next()) {
-//                Item item = new Item();
-//                item.setName(resultSet.getString("name"));
-//                item.setPrice(resultSet.getDouble("price"));
-//                items.add(item);
-//            }
-//        } catch (SQLException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return items;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+    
+    
     
 }
-    }
