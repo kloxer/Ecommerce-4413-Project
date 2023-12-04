e5d5185cba5a98f4aa7635c6c67f42e0ac93b8c2
davzakh <91906562+davzakh@users.noreply.github.com>
2023-12-03 21:38:11 -0500

    Add files via upload
    Made ItemDAOImpl like BookDAOImpl from lab 6 using our DB.

--- Ecommerce Project/src/main/java/dao/ItemDAO.java
+++ Ecommerce Project/src/main/java/dao/ItemDAO.java
@@ -1,8 +1,18 @@
 package dao;
 
+import java.sql.SQLException;
 import java.util.List;
 import model.Item;
 
 public interface ItemDAO {
-    List<Item> getAllItems();
+
+	List<Item> findAllItems();
+
+	List<Item> searchItemsByKeyword(String keyWord);
+
+	List<Item> findItemsByCategory(String category);
+
+	void insert(Item item);
+
+	void delete(String itemID);
 }
--- Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
+++ Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
@@ -1,52 +1,181 @@
 package dao;
 
-import model.Item;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+
+import model.Item;
 
 public class ItemDAOImpl implements ItemDAO {
 
-    private ServletContext context;
+    private DataSource dSource;
 
-    public ItemDAOImpl(ServletContext context) {
-        this.context = context;
+    public ItemDAOImpl() throws NamingException{
+    	Context ctx = new InitialContext();
+    	dSource = (DataSource) ctx.lookup("java:comp/env/jdbc/ecomDB");
     }
 
     private Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new SQLException("MySQL Driver not found", e);
-        }
-        String url = "jdbc:mysql://ecomproj-ecomfin.a.aivencloud.com:18620/defaultdb";
-        String user = "avnadmin";
-        String password = "AVNS_4En2QrrZ4DU5tpub8TB";
-        return DriverManager.getConnection(url, user, password);
+    	return dSource.getConnection();
     }
 
-    
-    public List<Item> getAllItems() {
-        List<Item> items = new ArrayList<>();
-        try (Connection connection = getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM Item")) {
+    private void closeConnection(Connection connection) {
+        if (connection == null)
+            return;
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Item> findAllItems() {
+        List<Item> result = new ArrayList<>();
+
+        String sql = "SELECT * FROM Item";
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
             while (resultSet.next()) {
                 Item item = new Item();
+                item.setItemID(resultSet.getString("itemID"));
                 item.setName(resultSet.getString("name"));
-                item.setPrice(resultSet.getDouble("price"));
-                items.add(item);
+                item.setDescription(resultSet.getString("description"));
+                item.setCategory(resultSet.getString("category"));
+                item.setBrand(resultSet.getString("brand"));
+                item.setQuantity(resultSet.getInt("quantity"));
+                item.setPrice(resultSet.getInt("price"));
+
+                result.add(item);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection(connection);
         }
-        return items;
+        return result;
     }
-    
-    
-    
-    
-    
+
+    @Override
+    public List<Item> searchItemsByKeyword(String keyWord) {
+        List<Item> result = new ArrayList<>();
+
+        String sql = "SELECT * FROM Item WHERE name LIKE ? OR description LIKE ? OR brand LIKE ?";
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            String keywordParam = "%" + keyWord + "%";
+            statement.setString(1, keywordParam);
+            statement.setString(2, keywordParam);
+            statement.setString(3, keywordParam);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Item item = new Item();
+                item.setItemID(resultSet.getString("itemID"));
+                item.setName(resultSet.getString("name"));
+                item.setDescription(resultSet.getString("description"));
+                item.setCategory(resultSet.getString("category"));
+                item.setBrand(resultSet.getString("brand"));
+                item.setQuantity(resultSet.getInt("quantity"));
+                item.setPrice(resultSet.getInt("price"));
+
+                result.add(item);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findItemsByCategory(String category) {
+        List<Item> result = new ArrayList<>();
+
+        String sql = "SELECT * FROM Item WHERE category = ?";
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, category);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Item item = new Item();
+                item.setItemID(resultSet.getString("itemID"));
+                item.setName(resultSet.getString("name"));
+                item.setDescription(resultSet.getString("description"));
+                item.setCategory(resultSet.getString("category"));
+                item.setBrand(resultSet.getString("brand"));
+                item.setQuantity(resultSet.getInt("quantity"));
+                item.setPrice(resultSet.getInt("price"));
+
+                result.add(item);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+        return result;
+    }
+
+    @Override
+    public void insert(Item item) {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO Item (itemID, name, description, category, brand, quantity, price) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            statement.setString(1,  String.valueOf(item.getItemID()));
+            statement.setString(2, item.getName());
+            statement.setString(3, item.getDescription());
+            statement.setString(4,  String.valueOf(item.getCategory()));
+            statement.setString(5, item.getBrand());
+            statement.setInt(6, (int) item.getQuantity());
+            statement.setInt(7, (int) item.getPrice());
+
+            statement.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    @Override
+    public void delete(String itemID) {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM Item WHERE itemID=?");
+            statement.setString(1, itemID);
+            statement.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+    }
 }
