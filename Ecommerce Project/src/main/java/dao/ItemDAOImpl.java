3c17a426711ab2d09f2a1038d41f99b254e4c509
kloxer <102638907+kloxer@users.noreply.github.com>
2023-12-03 19:18:31 -0500

    Adding db connection

--- Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
+++ Ecommerce Project/src/main/java/dao/ItemDAOImpl.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
@@ -34,19 +36,49 @@ public class ItemDAOImpl implements ItemDAO{
     }
 
     public List<Item> getAllItems() {
-        List<Item> items = new ArrayList<>();
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Item");
-             ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                Item item = new Item();
-                item.setName(resultSet.getString("name"));
-                item.setPrice(resultSet.getDouble("price"));
-                items.add(item);
+	    List<Item> items = new ArrayList<>();
+    	try {
+    	    // Load the JDBC driver class
+    	    Class.forName("com.mysql.cj.jdbc.Driver"); // This is often optional in newer versions of JDBC
+
+    	    // Set your database credentials and URL
+    	    String url = "jdbc:mysql://ecomproj-ecomfin.a.aivencloud.com:18620/defaultdb";
+    	    String user = "avnadmin";
+    	    String password = "AVNS_4En2QrrZ4DU5tpub8TB";
+
+
+    	    // Create a connection to the database
+    	    Connection connection = DriverManager.getConnection(url, user, password);
+    	    
+    	    // Create a statement object to execute queries
+    	    PreparedStatement statement = connection.prepareStatement("SELECT * FROM Item");
+    	    ResultSet resultSet = statement.executeQuery()); {
+                while (resultSet.next()) {
+                    Item item = new Item();
+                    item.setName(resultSet.getString("name"));
+                    item.setPrice(resultSet.getDouble("price"));
+                    items.add(item);
+                }
+            } 
+    	    catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return items;
-    }
+            return items;
+    	    
+//        List<Item> items = new ArrayList<>();
+//        try (Connection connection = getConnection();
+//             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Item");
+//             ResultSet resultSet = statement.executeQuery()) {
+//            while (resultSet.next()) {
+//                Item item = new Item();
+//                item.setName(resultSet.getString("name"));
+//                item.setPrice(resultSet.getDouble("price"));
+//                items.add(item);
+//            }
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return items;
+    
 }
+    }
