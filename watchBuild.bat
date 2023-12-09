b5eba15dba2d2f10b3fe01f364a4d6279b0f3460
kloxer <102638907+kloxer@users.noreply.github.com>
2023-12-08 22:54:16 -0500

    Ignore the bat file, need to refresh build using vscode

---
+++ watchBuild.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\Users\rjona\Proton Drive\jonathan.a.r\My files\vscode\git2\Ecommerce-4413-Project\Ecommerce Project
+nodemon --watch src -e java,xml,jsp --exec "mvn clean package"
