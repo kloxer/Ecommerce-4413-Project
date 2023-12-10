ad91a5cce92d72c17a47d3caa1f706a53cf0c4d7
davzakh <91906562+davzakh@users.noreply.github.com>
2023-12-10 00:13:35 -0500

    Add files via upload
    Models for Purchase and Purchase_Item in DB.

---
+++ Ecommerce Project/src/main/java/model/Purchase.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Purchase implements Serializable {
+    
+	private static final long serialVersionUID = 1L;
+	private int purchaseId;
+    private int userId;
+    private int addressId;
+    private Date date;
+    private double total;
+
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
