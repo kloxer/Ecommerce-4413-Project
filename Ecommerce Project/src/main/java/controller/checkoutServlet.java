a044e9b30f068a05888659172bcf808e8c03f5db
kloxer <102638907+kloxer@users.noreply.github.com>
2023-12-18 17:00:36 -0500

    Managed to send stuff from servlet to orderServlet. Working on implem.

--- Ecommerce Project/src/main/java/controller/checkoutServlet.java
+++ Ecommerce Project/src/main/java/controller/checkoutServlet.java
@@ -1,5 +1,7 @@
 package controller;
 import java.io.IOException;
+
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.AddressDAO;
+import dao.AddressDAOImpl;
+import model.Address;
 import model.Cart;
+import model.User;
 
 
 
@@ -29,8 +35,36 @@ public class checkoutServlet extends HttpServlet {
         
         Cart cart = (Cart) session.getAttribute("cart"); //get cart from session, assume already in session
 
+        User user = (User) session.getAttribute("user");
+
+        String paymentInfo = request.getParameter("paymentInfo");
+        int addressInfo = Integer.parseInt(request.getParameter("addressInfo")); // Parse the string value to an integer
+        int userId = user.getUserId();
+        
+        AddressDAO addressDAO = null;
+
+        try {
+            addressDAO = new AddressDAOImpl();
+        } catch (NamingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Address address = addressDAO.getAddressBasedOnID(addressInfo);
+        String currAddr = address.getAddressLine1();
+
+        System.out.println("Payment Info: " + paymentInfo);
+        System.out.println("AddressID Info: " + addressInfo);
+        System.out.println("UserID Info: " + addressInfo);
+        System.out.println("street line Info: " + currAddr);
+
+        
+        
+    
+
+
+
          // After processing the order, redirect to showorder.jsp
-            response.sendRedirect("index.jsp");
+        response.sendRedirect("index.jsp");
 
 
 
